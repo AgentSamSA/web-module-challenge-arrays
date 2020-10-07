7e2cacf4cb00100de93f8205caf59625dde9cc9d
Samuel Lee <samuelxlee@gmail.com>
2020-10-07 15:02:27 -0700

    finished stretch task 2 for getting random flavors

--- index.js
+++ index.js
@@ -35,6 +35,87 @@ const originalFlavors = [
     "Vanilla Burnt Almond"
 ]
 
+// NEW DATA ARRAYS FOR STRETCH 2 ⬇️
+const newFlavors = [
+    "Date night",
+    "U.S.S Butterscotch (Stranger Things special)",
+    "Honey Almond",
+    "Mint Chocolate Chip",
+    "Chocolate",
+    "Oreo® Cookies'n Cream",
+    "Chocolate Chip",
+    "Pralines 'n Cream",
+    "Very Berry Strawberry",
+    "Chocolate Chip Cookie Dough",
+    "Old Fashioned Butter Pecan",
+    "Jamoca®",
+    "Jamoca® Almond Fudge",
+    "Reese's® Peanut Butter Cup",
+    "Rocky Road",
+    "Peanut Butter ’n Chocolate",
+    "Gold Medal Ribbon®",
+    "World Class® Chocolate",
+    "Cherries Jubilee",
+    "Chocolate Fudge",
+    "Daiquiri Ice",
+    "Rainbow Sherbet",
+    "Rainbow Swirl"
+]
+
+const seasonalFlavors = [
+    "America's Birthday Cake",
+    "Baseball Nut®",
+    "Blueberry Cheesecake",
+    "Bourbon Street Pecan Pie",
+    "Brownie Bar Mashup",
+    "Cherry Cordial with Kisses",
+    "Chocolate Mousse Royale",
+    "French Vanilla",
+    "Eggnog",
+    "German Chocolate Cake",
+    "Icing on the Cake",
+    "Love Potion #31",
+    "New York Cheesecake",
+    "Nutty Coconut",
+    "Peppermint",
+    "Strawberry Cheesecake",
+    "Rock ’n Pop Swirl",
+    "Reese’s Peanut Butter Cup",
+    "Trick Oreo Treat",
+    "Winter White Chocolate",
+    "made with Snickers®",
+    "made with M&M's®",
+    "Heath®",
+    "Mango Tango"
+]
+
+const regionalFlavors = [
+    "Pink Bubblegum",
+    "Caramel Macchiato",
+    "York Peppermint Pattie",
+    "Cotton Candy",
+    "Orange Sherbet",
+    "Grape Ice",
+    "Watermelon Ice",
+    "Miami Vice Sorbet",
+    "Splish Splash®",
+    "Wild 'n Reckless Sherbet",
+    "Lemon Custard",
+    "Oregon Blackberry",
+    "Bananas ‘n Strawberries",
+    "Mississippi Mud",
+    "Rum Raisin",
+    "Creole Cream Cheese",
+    "Chocolate Almond",
+    "Fudge Brownie",
+    "Banana Nut",
+    "Black Walnut",
+    "Cotton Candy Crackle",
+    "Quarterback Crunch",
+    "Chocolate Chocolate Chip Cheesecake",
+    "Caramel 'n' Cookies"
+]
+
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: Copy the Array! 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 We have an array called originalFlavors with 31 flavors (see above).  In these tasks, we will be reading and writing data to this array.  
 With all of these changes going on, we don't want to lose track of the actual, original 31 flavors.  So we need to copy the original array!
@@ -189,17 +270,13 @@ Use the getAverageWordLength function below to do the following:
 
 function getAverageWordLength(array) {
     let wordTotal = 0;
-    let avgWords = 0;
-    let words = [];
     for (let i = 0; i < array.length; i++) {
-        words = array[i].split(" ");
+        let words = array[i].split(" ");
         wordTotal += words.length;
-        avgWords = wordTotal/array.length;
     }
-    return Math.ceil(avgWords);
+    return Math.round(wordTotal/array.length);
 }
 console.log(getAverageWordLength(originalFlavors));
-
 
 /* 💪💪💪💪💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪💪💪💪
 Baskin Robins now offers new flavors, seasonal flavors, and even regional flavors. Write a function that will randomly select a total of 31 flavors 
@@ -213,107 +290,16 @@ Use the getRandomFlavors function and new arrays below to do the following:
     For example: getRandomFlavors(originalFlavors, newFlavors, seasonalFlavors, regionalFlavors) might return ["Strawberry Cheesecake", "Eggnog,"..."Chocolate"].
 */
 
-/*function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
 function getRandomFlavors(array1, array2, array3, array4) {
-    const min = 0;
-    const max = 31;
     const randomFlavors = [];
     const randomFlavorGroup = [array1, array2, array3, array4];
-    for (let i = 0; i < randomFlavorGroup.length; i++) {
-        let pickedFlavors = randomFlavorGroup[Math.floor(Math.random() * (4))];
-        for (let i = 0; )
+    for (let i = 0; i < 31; i++) {
+        let pickedFlavors = randomFlavorGroup[Math.floor(Math.random() * randomFlavorGroup.length)];
+        randomFlavors.push(pickedFlavors[Math.floor(Math.random() * pickedFlavors.length)]);
     }
-}*/
-
-function getRandomFlavors() {
-
+    return randomFlavors;
 }
-
-// NEW DATA ARRAYS FOR STRETCH 2 ⬇️
-const newFlavors = [
-    "Date night",
-    "U.S.S Butterscotch (Stranger Things special)",
-    "Honey Almond",
-    "Mint Chocolate Chip",
-    "Chocolate",
-    "Oreo® Cookies'n Cream",
-    "Chocolate Chip",
-    "Pralines 'n Cream",
-    "Very Berry Strawberry",
-    "Chocolate Chip Cookie Dough",
-    "Old Fashioned Butter Pecan",
-    "Jamoca®",
-    "Jamoca® Almond Fudge",
-    "Reese's® Peanut Butter Cup",
-    "Rocky Road",
-    "Peanut Butter ’n Chocolate",
-    "Gold Medal Ribbon®",
-    "World Class® Chocolate",
-    "Cherries Jubilee",
-    "Chocolate Fudge",
-    "Daiquiri Ice",
-    "Rainbow Sherbet",
-    "Rainbow Swirl"
-]
-
-const seasonalFlavors = [
-    "America's Birthday Cake",
-    "Baseball Nut®",
-    "Blueberry Cheesecake",
-    "Bourbon Street Pecan Pie",
-    "Brownie Bar Mashup",
-    "Cherry Cordial with Kisses",
-    "Chocolate Mousse Royale",
-    "French Vanilla",
-    "Eggnog",
-    "German Chocolate Cake",
-    "Icing on the Cake",
-    "Love Potion #31",
-    "New York Cheesecake",
-    "Nutty Coconut",
-    "Peppermint",
-    "Strawberry Cheesecake",
-    "Rock ’n Pop Swirl",
-    "Reese’s Peanut Butter Cup",
-    "Trick Oreo Treat",
-    "Winter White Chocolate",
-    "made with Snickers®",
-    "made with M&M's®",
-    "Heath®",
-    "Mango Tango"
-]
-
-const regionalFlavors = [
-    "Pink Bubblegum",
-    "Caramel Macchiato",
-    "York Peppermint Pattie",
-    "Cotton Candy",
-    "Orange Sherbet",
-    "Grape Ice",
-    "Watermelon Ice",
-    "Miami Vice Sorbet",
-    "Splish Splash®",
-    "Wild 'n Reckless Sherbet",
-    "Lemon Custard",
-    "Oregon Blackberry",
-    "Bananas ‘n Strawberries",
-    "Mississippi Mud",
-    "Rum Raisin",
-    "Creole Cream Cheese",
-    "Chocolate Almond",
-    "Fudge Brownie",
-    "Banana Nut",
-    "Black Walnut",
-    "Cotton Candy Crackle",
-    "Quarterback Crunch",
-    "Chocolate Chocolate Chip Cheesecake",
-    "Caramel 'n' Cookies"
-]
-
-
+console.log(getRandomFlavors(originalFlavors, seasonalFlavors, newFlavors, regionalFlavors));
 
 /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
 function foo() {
